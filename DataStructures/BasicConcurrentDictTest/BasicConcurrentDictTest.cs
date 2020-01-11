9f89e81d529a28433a3662558c45010b434f867c
riach <riach@microsoft.com>
2020-01-11 14:57:03 -0800

    basic concurrent dict

---
+++ DataStructures/BasicConcurrentDictTest/BasicConcurrentDictTest.cs
@@ -0,0 +1,69 @@
+using ConcurrentDictionary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicConcurrentDictTest
+{
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class BasicConcurrentDictTest
+    {
+        [TestMethod]
+        public void BasicAdd()
+        {
+            var dict = new BasicCocurrentDict();
+            dict.AddOrUpdate("r", 1);
+            var value = dict.Get("r");
+            Assert.AreEqual(1, value);
+            dict.AddOrUpdate("o", 2);
+            value = dict.Get("r");
+            Assert.AreEqual(1, value);
+            value = dict.Get("o");
+            Assert.AreEqual(2, value);
+        }
+
+        [TestMethod]
+        public void BasicUpdate()
+        {
+            var dict = new BasicCocurrentDict();
+            dict.AddOrUpdate("r", 1);
+            var value = dict.Get("r");
+            Assert.AreEqual(1, value);
+            dict.AddOrUpdate("r", 20);
+            value = dict.Get("r");
+            Assert.AreEqual(20, value);
+        }
+
+
+        [TestMethod]
+        public void ConcurrentGet()
+        {
+            var dict = new BasicCocurrentDict();
+            var numTasks = 128;
+            dict.AddOrUpdate("r", 1);
+            Parallel.For(0, numTasks, (index) =>
+            {
+                var value = dict.Get("r");
+                Assert.AreEqual(1, value);
+            });
+
+        }
+
+        [TestMethod]
+        public void ConcurrentUpdate()
+        {
+            var dict = new BasicCocurrentDict();
+            var numTasks = 128;
+            dict.AddOrUpdate("r", 1);
+            int lastThread = 0;
+            Parallel.For(0, numTasks, (index) =>
+            {
+                dict.AddOrUpdate("r", index);
+                lastThread = index;
+            });
+            var value = dict.Get("r");
+            Assert.AreEqual(lastThread, value);
+
+        }
+    }
+}
---
+++ DataStructures/ConcurrentDictionary/BasicCocurrentDict.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ConcurrentDictionary
+{
+    /// <summary>
+    /// This is a basic concurrent dictionary. We don't handle expanding the array
+    /// or hash collisions here and we only handle int values
+    /// </summary>
+    public class BasicCocurrentDict
+    {
+        /// <summary>
+        /// This is the total size of our dictionary. We can expand arrays later for more real use cases
+        /// </summary>
+        private const int _size = 1024; 
+        /// <summary>
+        /// How many items in each segment or chunk for locking
+        /// </summary>
+        private const int _segmentSize = 8;
+        /// <summary>
+        /// The array for the dictionary
+        /// </summary>
+        private int[] _array = new Int32[_size];
+        /// <summary>
+        /// Array of locks for concurrency
+        /// </summary>
+        private object[] _locks = new Object[_size / _segmentSize];
+
+        /// <summary>
+        /// Default constructor, just initializes the objects
+        /// </summary>
+        public BasicCocurrentDict()
+        {
+            //initialize the value array with -1
+            for (int i = 0; i < _size; i++)
+            {
+                _array[i] = -1;
+            }
+            //initialize the locks with empty objects
+            var lockSize = _size / _segmentSize;
+            for (int i = 0; i < lockSize; i++)
+            {
+                _locks[i]= new object();
+            }
+        }
+
+        /// <summary>
+        /// Adds or updates the value of an item in the dictionary
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value for the key</param>
+        public void AddOrUpdate(string key, int value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new Exception($"Key cannot be null, empty or whitespace");
+            }
+
+            var index = GetHashIndex(key);
+            
+            var lockIndex = index / _segmentSize;
+            lock (_locks[lockIndex])
+            {
+                _array[index] = value;
+            }
+
+        }
+
+        /// <summary>
+        /// Adds or updates the value of an item in the dictionary
+        /// </summary>
+        /// <param name="key">Key</param>
+        public int Get(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new Exception($"Key cannot be null, empty or whitespace");
+            }
+
+            var index = GetHashIndex(key);
+            var lockIndex = index / _segmentSize;
+            lock (_locks[lockIndex])
+            {
+               return _array[index];
+            }
+
+        }
+
+        private int GetHashIndex(string key)
+        {
+            //not great but we're pretending there's no hash collision
+            return Math.Abs(key.GetHashCode() % _size);
+        }
+    }
+}
